24046deb64e6818dc3a03ce019c15266b8f78af9
Igor Belo <igorcoura@gmail.com>
2016-12-06 18:56:55 -0200

    allow code before resource: imports, etc

--- program.py
+++ program.py
@@ -36,18 +36,21 @@ class Context:
             test.run(self.before, self.after)
 
 class Test:
-    def __init__(self, context, test):
+    def __init__(self, context, test, code_before_resource):
+        self.code_before_resource = code_before_resource
         self.context = context
         self.test = test
 
     def run(self, before_callbacks, after_callbacks):
         body = self.test.body
 
-        def test_function(self):
+        def test_function(inner_self):
+            exec_code(self.code_before_resource, inner_self)
+
             for before in before_callbacks:
                 exec_code(before.body)
 
-            exec_code(body, self)
+            exec_code(body, inner_self)
 
             for after in after_callbacks:
                 exec_code(after.body)
@@ -64,6 +67,7 @@ class Test:
 class Flow:
     def __init__(self, *args, **kwargs):
         self.contexts = []
+        self.code_before_resource = kwargs['code_before_resource']
 
     def build(self, command):
         if type(command).__name__ == 'ContextCommand':
@@ -99,6 +103,7 @@ class Flow:
                     context.after.append(command)
             else:
                 test = Test(
+                    code_before_resource = self.code_before_resource,
                     context = context,
                     test = command
                 )
@@ -109,13 +114,21 @@ class Flow:
     def run(self):
         [context.run() for context in self.contexts]
 
+def build_and_run_flow(resource, code_before_resource = []):
+    for command in resource.commands:
+        flow = Flow(code_before_resource = code_before_resource)
+        flow.build(command)
+        flow.run()
+
 mm = metamodel_from_file('grammar.tx')
 model = mm.model_from_file('program.api')
 
-for command in model.commands:
-    flow = Flow()
-    flow.build(command)
-    flow.run()
+if model.header_codes:
+    code_before_resource = [header_code for header_code in model.header_codes if type(header_code) == unicode]
+    resource = model.header_codes[len(code_before_resource)]
+    build_and_run_flow(resource, code_before_resource)
+else:
+    build_and_run_flow(model.resource)
 
 if __name__ == '__main__':
     unittest.main()
